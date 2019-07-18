ddd6dff5502199dc78217805293edd32a3f5cf89
Dániel Kántor <git@daniel-kantor.com>
2019-07-18 20:40:40 +0200

    Code style cleanups

--- src/cx/exceptions.py
+++ src/cx/exceptions.py
@@ -1,3 +1,2 @@
 class UnknownCurrencyError(Exception):
     pass
-
--- src/cx/logic.py
+++ src/cx/logic.py
@@ -1,26 +1,28 @@
 from cx.exceptions import UnknownCurrencyError
 
-valid_currencies = {"EUR", "USD", "CZK", "PLN"}
-base_currency = "EUR"
-assert base_currency in valid_currencies
+VALID_CURRENCIES = {"EUR", "USD", "CZK", "PLN"}
+BASE_CURRENCY = "EUR"
+assert BASE_CURRENCY in VALID_CURRENCIES
+
 
 def get_base_rate(db, date, to):
-    pass
+    "Currency conversion the base currency and another currency on a given date"
+
 
 def get_arbitrary_rate(db, date, from_, to):
-    if from_ not in valid_currencies:
+    "Currency conversion between any two currencies on a given date"
+
+    if from_ not in VALID_CURRENCIES:
         raise UnknownCurrencyError('Currency "{}" is unknown'.format(from_))
-    if to not in valid_currencies:
+    if to not in VALID_CURRENCIES:
         raise UnknownCurrencyError('Currency "{}" is unknown'.format(to))
     if from_ == to:
         return 1.0
 
-    if from_ == base_currency:
+    if from_ == BASE_CURRENCY:
         return get_base_rate(db=db, date=date, to=to)
 
-    if to == base_currency:
+    if to == BASE_CURRENCY:
         return 1 / get_base_rate(db=db, date=date, to=from_)
 
     return get_base_rate(db=db, date=date, to=to) / get_base_rate(db=db, date=date, to=from_)
-    
-
