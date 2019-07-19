487aaa1aad181e3a3f877895e5be36a313be6090
Dániel Kántor <git@daniel-kantor.com>
2019-07-19 18:30:19 +0200

    Add pricedb parsing utilities

---
+++ src/cx/pricedb.py
@@ -0,0 +1,24 @@
+import datetime
+from collections import defaultdict
+from cx.dates import parse_date
+
+
+def parse_line(input_):
+    parts = input_.split(' ')
+    raw_date = parts[1]
+    raw_rate = parts[3]
+    currency = parts[4]
+    date = parse_date(raw_date)
+    rate = float(raw_rate)
+
+    return (date, rate, currency)
+
+
+def build_db(lines):
+    results = defaultdict(dict)
+
+    for line in lines:
+        date, rate, currency = line
+        results[date].update({currency: rate})
+
+    return results
---
+++ tests/test_pricedb.py
@@ -0,0 +1,36 @@
+import pytest
+import datetime
+from cx.pricedb import parse_line, build_db
+
+examples = (
+    ("P 2019-07-18 EUR 1.1216 USD", datetime.date(2019, 7, 18), 1.1216, "USD"),
+    ("P 2016-04-08 EUR 120.89 JPY", datetime.date(2016, 4, 8), 120.89, "JPY"),
+    ("P 2019-07-18 EUR 1.9558 BGN", datetime.date(2019, 7, 18), 1.9558, "BGN"),
+    ("P 2017-11-08 EUR 25.586 CZK", datetime.date(2017, 11, 8), 25.586, "CZK"),
+)
+
+
+@pytest.mark.parametrize('input_,date,rate,currency', examples)
+def test_parse_line(input_, date, rate, currency):
+    assert parse_line(input_) == (date, rate, currency)
+
+
+def test_build_db_empty_input():
+    assert build_db([]) == dict()
+
+
+def test_build_db_single_input():
+    assert build_db([examples[0][1:]]) == {
+        datetime.date(2019, 7, 18): {"USD": 1.1216}}
+
+
+def test_build_db_same_date():
+    assert build_db([examples[0][1:], examples[2][1:]]) == {
+        datetime.date(2019, 7, 18): {"USD": 1.1216, "BGN": 1.9558}}
+
+
+def test_build_db_different_date():
+    assert build_db([examples[0][1:], examples[1][1:]]) == {
+        datetime.date(2019, 7, 18): {"USD": 1.1216},
+        datetime.date(2016, 4, 8): {"JPY": 120.89}
+    }
