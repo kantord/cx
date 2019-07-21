953894e115fd08020af2bb9df0bf6046a439bdbb
Dániel Kántor <git@daniel-kantor.com>
2019-07-21 13:37:43 +0200

    Refactor: move database load logic to a separate method

--- src/cx/db.py
+++ src/cx/db.py
@@ -3,6 +3,9 @@ from cx.common.pricedb import parse_line, build_db
 
 class Database():
     def __init__(self):
+        self.update()
+
+    def update(self):
         with open('./price.db') as input_file:
             self.data = build_db(map(parse_line, input_file))
 
